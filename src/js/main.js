e8decf455c3cd1b108924897bd480fbad1054b7a
Piotr Sienkiewicz <suniciu@piasta.pl>
2020-04-07 12:20:51 +0200

    feat: new building system

--- src/js/main.js
+++ src/js/main.js
@@ -17,6 +17,28 @@ if ('serviceWorker' in navigator) {
 // place your code below
 
 
-console.log(`Hello world!`);
+console.log('siema');
+const name = 'Piotr'
+const age = 29
+console.log(name);
+console.log(age);
+
+console.log(`Nazywam się ${name} i mam ${age} lat`);
+
+console.log ('siema siema ziomek ❤')
+
+const about = document.querySelector('.about__paragraph--js');
+
+console.log(about.innerHTML)
+
+about.innerHTML = `Nazywam się ${name} i mam ${age} lat`
+
+
+const navigationSwitcher = document.querySelector('.navigation__switcher--js')
+
+navigationSwitcher.addEventListener('click' , (e) => {
+    const navigationList = document.querySelector('.navigation__list--js')
+    navigationList.classList.toggle('navigation__list--visible')
+})
 
 
